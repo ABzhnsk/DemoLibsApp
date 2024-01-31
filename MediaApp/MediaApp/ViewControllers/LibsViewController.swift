8cbb9a7ec4ffa975048e642003af9b1d542108ba
ABzhnsk <annabuzch@gmail.com>
2024-01-31 15:17:20 +0300

    make LibsViewController better

--- MediaApp/MediaApp/ViewControllers/LibsViewController.swift
+++ MediaApp/MediaApp/ViewControllers/LibsViewController.swift
@@ -5,12 +5,15 @@
 //  Created by Anna Buzhinskaya on 11.01.2024.
 //
 
+
 import UIKit
+
 
 class LibsViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Libraries"
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -33,21 +36,21 @@ class LibsViewController: UITableViewController {
                 cabbageVideoViewController.title = libsName[0]
                 navigationController?.pushViewController(cabbageVideoViewController, animated: true)
             case 1:
-                let nytPhotoViewerViewController = NYTPhotoViewerViewController()
-                nytPhotoViewerViewController.title = libsName[1]
-                navigationController?.pushViewController(nytPhotoViewerViewController, animated: true)
-            case 2:
-                let lightboxPhotoViewController = LightboxPhotoViewController()
-                lightboxPhotoViewController.title = libsName[2]
-                navigationController?.pushViewController(lightboxPhotoViewController, animated: true)
-            case 3:
                 let lightboxVideoViewController = LightboxVideoViewController()
-                lightboxVideoViewController.title = libsName[3]
+                lightboxVideoViewController.title = libsName[1]
                 navigationController?.pushViewController(lightboxVideoViewController, animated: true)
-            case 4:
+            case 2:
                 let agrumePhotoViewController = AgrumePhotoViewController()
-                agrumePhotoViewController.title = libsName[4]
+                agrumePhotoViewController.title = libsName[2]
                 navigationController?.pushViewController(agrumePhotoViewController, animated: true)
+            case 3:
+                let lightboxPhotoViewController = LightboxPhotoViewController()
+                lightboxPhotoViewController.title = libsName[3]
+                navigationController?.pushViewController(lightboxPhotoViewController, animated: true)
+            case 4:
+                let nytPhotoViewerViewController = NYTPhotoViewerViewController()
+                nytPhotoViewerViewController.title = libsName[4]
+                navigationController?.pushViewController(nytPhotoViewerViewController, animated: true)
             default:
                 navigationController?.pushViewController(CabbageVideoViewController(), animated: true)
         }
@@ -55,11 +58,10 @@ class LibsViewController: UITableViewController {
 
     private let libsName = [
         "Cabbage - video",
-        "NYTPhotoViewer - photo",
-        "Lightbox - photo",
         "Lightbox - video",
-        "Agrume - photo"
+        "Agrume - photo",
+        "Lightbox - photo",
+        "NYTPhotoViewer - photo",
     ]
-
 } // class LibsViewController
 
